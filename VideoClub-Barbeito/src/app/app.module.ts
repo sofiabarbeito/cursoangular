a0f67cf1283f34185968dc5591ec2a30e0592930
Sofia Barbeito <sofiabarbeito@gmail.com>
2021-12-06 17:06:53 -0300

    creacion componentes

--- VideoClub-Barbeito/src/app/app.module.ts
+++ VideoClub-Barbeito/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ListadoComponent } from './listado/listado.component';
+import { InfoComponent } from './info/info.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    ListadoComponent,
+    InfoComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule
